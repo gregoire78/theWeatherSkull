ff1e0dd0e2c72ab655f0b9c8d1e2d9043e27a1d7
Grégoire <greg.joncour@gmail.com>
2016-03-25 23:50:56 +0100

    background image

--- app/pages/page1/page1.js
+++ app/pages/page1/page1.js
@@ -1,22 +1,17 @@
 import {Page} from 'ionic-angular';
 import {NavButton} from './navButton';
+import {Date} from './Date';
+import {Geolocation} from 'ionic-native';
 
 @Page({
     templateUrl: 'build/pages/page1/page1.html',
-    directives: [NavButton]
+    directives: [NavButton, Date]
 })
 export class Page1 {
     constructor() {
-        navigator.geolocation.getCurrentPosition(function (position) {
-            alert(
-                'Latitude: ' + position.coords.latitude + '\n' +
-                'Longitude: ' + position.coords.longitude + '\n' +
-                'Altitude: ' + position.coords.altitude + '\n' +
-                'Accuracy: ' + position.coords.accuracy + '\n' +
-                'Altitude Accuracy: ' + position.coords.altitudeAccuracy + '\n' +
-                'Heading: ' + position.coords.heading + '\n' +
-                'Speed: ' + position.coords.speed + '\n' +
-                'Timestamp: ' + position.timestamp + '\n')
+        Geolocation.getCurrentPosition().then((resp) => {
+            this.pos = resp.coords.latitude;
+            this.pos1 = resp.coords.longitude
         });
     }
 }
