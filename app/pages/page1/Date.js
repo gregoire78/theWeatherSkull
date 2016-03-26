439aaf4f08f870d82f3863604ea2acef997a4000
Grégoire <greg.joncour@gmail.com>
2016-03-26 15:28:43 +0100

    date

---
+++ app/pages/page1/Date.js
@@ -0,0 +1,13 @@
+/**
+ * Created by gregoire on 25/03/2016.
+ */
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'date',
+    template: `<div></div>`
+})
+export class Date {
+    constructor(){
+    }
+}
