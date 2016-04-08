64e1261b2e01588b4d8c13acd201d8b1d4e5409d
Grégoire <greg.joncour@gmail.com>
2016-04-08 17:54:24 +0200

    map search

--- app/pages/page2/page2.js
+++ app/pages/page2/page2.js
@@ -12,7 +12,8 @@ import {Http} from 'angular2/http';
 </ion-toolbar>
 
 <ion-content class="page2">
-    <input (click)="load()" id="dede" autocapitalize="off" autocomplete="off" autocorrect="off" class="searchbar-input" spellcheck="false" type="search" placeholder="Search">
+    <!--<input (click)="load()" id="dede" autocapitalize="off" autocomplete="off" autocorrect="off" class="searchbar-input" spellcheck="false" type="search" placeholder="Search">-->
+    <input (click)="load()" id="pac-input" autocapitalize="off" autocomplete="off" autocorrect="off" type="text" placeholder="Enter a location" spellcheck="false" type="search">
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAC4k0qIOxFSbHMZ6Du0sirVICthUVkwfU&libraries=places"></script>
     <div id="map"></div>  
 </ion-content>
@@ -27,16 +28,56 @@ export class Page2 {
 
   load(){
 
-    var autocomplete = new google.maps.places.Autocomplete((document.getElementById("dede")));
+    var autocomplete = new google.maps.places.Autocomplete((document.getElementById("pac-input")));
+      autocomplete.bindTo('bounds', this.map);
 
+      var infowindow = new google.maps.InfoWindow();
+      var marker = new google.maps.Marker({
+          map: this.map,
+          anchorPoint: new google.maps.Point(0, -29)
+      });
     autocomplete.addListener('place_changed', () => {
-      var lat = autocomplete.getPlace().geometry.location.lat();
-      var lng = autocomplete.getPlace().geometry.location.lng();
+        infowindow.close();
+        marker.setVisible(false);
+        var place = autocomplete.getPlace();
+    if (!place.geometry) {
+        window.alert("Autocomplete's returned place contains no geometry");
+        return;
+    }
+        var lat = place.geometry.location.lat();
+      var lng = place.geometry.location.lng();
       this.http.get('http://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+lng+'&units=metric&appid=f513805255c080947d4115bc85cf923e')
           .map(response => response.json())
           .subscribe(result => {
             console.log(result)
           });
+        if (place.geometry.viewport) {
+            this.map.fitBounds(place.geometry.viewport);
+        } else {
+            this.map.setCenter(place.geometry.location);
+            this.map.setZoom(17);  // Why 17? Because it looks good.
+        }
+        marker.setIcon(/** @type {google.maps.Icon} */({
+            url: place.icon,
+            size: new google.maps.Size(71, 71),
+            origin: new google.maps.Point(0, 0),
+            anchor: new google.maps.Point(17, 34),
+            scaledSize: new google.maps.Size(35, 35)
+        }));
+        marker.setPosition(place.geometry.location);
+        marker.setVisible(true);
+
+        var address = '';
+        if (place.address_components) {
+            address = [
+                (place.address_components[0] && place.address_components[0].short_name || ''),
+                (place.address_components[1] && place.address_components[1].short_name || ''),
+                (place.address_components[2] && place.address_components[2].short_name || '')
+            ].join(' ');
+        }
+
+        infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
+        infowindow.open(this.map, marker);
     });
   }
 
