cf2d4debf5a5ce207f2802fdc8f75ec273818a74
Grégoire <greg.joncour@gmail.com>
2016-04-09 01:23:08 +0200

    styled map

--- app/pages/page2/page2.js
+++ app/pages/page2/page2.js
@@ -119,7 +119,25 @@ export class Page2 {
     }
 
     loadMap() {
-        var styledMap = new google.maps.StyledMapType([{"featureType":"landscape","stylers":[{"hue":"#FFBB00"},{"saturation":43.400000000000006},{"lightness":37.599999999999994},{"gamma":1}]},{"featureType":"road.highway","stylers":[{"hue":"#FFC200"},{"saturation":-61.8},{"lightness":45.599999999999994},{"gamma":1}]},{"featureType":"road.arterial","stylers":[{"hue":"#FF0300"},{"saturation":-100},{"lightness":51.19999999999999},{"gamma":1}]},{"featureType":"road.local","stylers":[{"hue":"#FF0300"},{"saturation":-100},{"lightness":52},{"gamma":1}]},{"featureType":"water","stylers":[{"hue":"#0078FF"},{"saturation":-13.200000000000003},{"lightness":2.4000000000000057},{"gamma":1}]},{"featureType":"poi","stylers":[{"hue":"#00FF6A"},{"saturation":-1.0989010989011234},{"lightness":11.200000000000017},{"gamma":1}]}],
+        var styledMap = new google.maps.StyledMapType([{
+                "featureType": "landscape",
+                "stylers": [{"hue": "#FFBB00"}, {"saturation": 43.400000000000006}, {"lightness": 37.599999999999994}, {"gamma": 1}]
+            }, {
+                "featureType": "road.highway",
+                "stylers": [{"hue": "#FFC200"}, {"saturation": -61.8}, {"lightness": 45.599999999999994}, {"gamma": 1}]
+            }, {
+                "featureType": "road.arterial",
+                "stylers": [{"hue": "#FF0300"}, {"saturation": -100}, {"lightness": 51.19999999999999}, {"gamma": 1}]
+            }, {
+                "featureType": "road.local",
+                "stylers": [{"hue": "#FF0300"}, {"saturation": -100}, {"lightness": 52}, {"gamma": 1}]
+            }, {
+                "featureType": "water",
+                "stylers": [{"hue": "#0078FF"}, {"saturation": -13.200000000000003}, {"lightness": 2.4000000000000057}, {"gamma": 1}]
+            }, {
+                "featureType": "poi",
+                "stylers": [{"hue": "#00FF6A"}, {"saturation": -1.0989010989011234}, {"lightness": 11.200000000000017}, {"gamma": 1}]
+            }],
             {name: "météo"});
 
         let options = {timeout: 10000, enableHighAccuracy: true};
@@ -132,7 +150,7 @@ export class Page2 {
                     zoom: 12,
                     // mapTypeId: google.maps.MapTypeId.HYBRID,
                     mapTypeControlOptions: {
-                        mapTypeIds: [google.maps.MapTypeId.HYBRID, 'map_style']
+                         mapTypeIds: [google.maps.MapTypeId.TERRAIN, 'map_style']
                     }
                 };
 
